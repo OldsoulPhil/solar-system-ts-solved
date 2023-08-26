0c6150ecf14236dccdd7a5546285a69371abb32d
OldsoulPhil <brenenshea@gmail.com>
2023-08-26 14:49:41 -0400

    updated exercise 16

--- exercises/e16.ts
+++ exercises/e16.ts
@@ -1,7 +1,7 @@
 // see e16.md
 
 // DO NOT USE `any` for this, you will need to use generics in order to pass both the vitest tests and the tsc compiler
-export function find<T>(array: T[], callback: (input: T) => undefined) {
+export function find<T>(array: T[], callback: (input: T) => unknown) {
   for (let elem of array) {
     if (callback(elem)) {
       return elem;
